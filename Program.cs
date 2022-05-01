8563c059c0ca93c17f4f79135c4f6a49483afd20
lodashreload <icarusjapan@gmail.com>
2022-05-01 16:35:08 -0400

    trying to problem solve

--- Program.cs
+++ Program.cs
@@ -10,39 +10,6 @@ namespace TamagotchiAPI
 {
     public class Program
     {
-        static void DisplayGreeting()
-        {
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine("         Welcome to the TamagotchiAPI   ");
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine();
-        }
-
-        static string PromptForString(string prompt)
-        {
-            Console.Write(prompt);
-            var userInput = Console.ReadLine();
-
-            return userInput;
-        }
-
-        static int PromptForInteger(string prompt)
-        {
-            Console.Write(prompt);
-            int userInput;
-            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
-
-            if (isThisGoodInput)
-            {
-                return userInput;
-            }
-            else
-            {
-                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
-                return 0;
-            }
-        }
-        //----------------------------------------------------------------
         public static async Task Main(string[] args)
         {
             var host = Utilities.CreateWebHostBuilder(args).Build();
